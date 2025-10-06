544fcaae691f3c967e431059a4108b87b0b6e37a
Jhonatan <jhonatanjau98@gmail.com>
2025-10-06 17:43:28 -0300

    solution

--- React/aula11-exercicioFormulario/src/App.jsx
+++ React/aula11-exercicioFormulario/src/App.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 
 const App = () => {
+  const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+  const [cores, setCores] = React.useState([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
 
   return (
-    <div>
-    </div>
-  )
-
-}
+    <form>
+      {coresArray.map(cor => 
+        <label key={cor}>
+          <input
+            type="checkbox"
+            value={cor}
+            checked={handleChecked(cor)}
+            onChange={handleChange}
+          />
+          {cor}
+        </label>
+      )}
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
 
 export default App
