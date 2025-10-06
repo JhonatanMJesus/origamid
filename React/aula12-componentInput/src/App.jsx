fff21d3eb93bcc57ecc1851982e6f4b239ee826b
Jhonatan <jhonatanjau98@gmail.com>
2025-10-06 18:46:04 -0300

    aula

--- React/aula12-componentInput/src/App.jsx
+++ React/aula12-componentInput/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Input from './Form/input';
 
 const App = () => {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
 
   return (
-    <div>
-    </div>
+    <form>
+      <Input id="nome" label="nome" value={nome} setValue={setNome} required />
+      {nome}
+      <Input id="email" label="email" value={email} setValue={setEmail} />
+      {email}
+      <button>Enviar</button>
+    </form>
   )
 
 }
